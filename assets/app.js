0929c7dce0b6861915123189d77d58b0891b8178
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2022-11-14 22:50:38 +0530

    added files

---
+++ assets/app.js
@@ -0,0 +1,15 @@
+document.getElementsByClassName("fab fa-github")[0].addEventListener("click" , function(){
+    window.open("#");
+});
+document.getElementsByClassName("fab fa-github")[1].addEventListener("click" , function(){
+    window.open("#");
+});
+document.getElementsByClassName("fab fa-github")[2].addEventListener("click" , function(){
+    window.open("#");
+});
+document.getElementsByClassName("fab fa-github")[3].addEventListener("click" , function(){
+    window.open("#");
+});
+document.getElementsByClassName("fab fa-github")[4].addEventListener("click" , function(){
+    window.open("#");
+});
